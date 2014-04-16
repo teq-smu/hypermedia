bd3d8796c6e4b7525fcda738e20d7195563dbb5b
Sébastien Muller <smuller@tequilarapido.com>
2014-04-16 16:41:48 +0200

    Init repo w/ index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+var express   = require('express');
+var Datastore = require('nedb');
+
+// ------------------------------------------------------------------------------------------------
+
+var app = express();
+
+app.use(express.bodyParser());
+
+//app.use(express.json());
+//app.use(express.urlencoded());
+
+// ------------------------------------------------------------------------------------------------
+
+var db    = {};
+db.movies = new Datastore({ filename: 'db/movies', autoload: true });
+
+// ------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
+
+app
+.get('/', function(req, res) {
+	res.send("The API is working.");
+})
+.post('/rpc', function(req, res) {
+	var body = req.body;
+	switch (body.action) {
+		case 'getMovies':
+			res.send("Requested API getMovies");
+			break;
+	}
+})
+.listen(3000);
