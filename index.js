12e512ff034f388e708a2301c7e6f839015ae8b9
Sébastien Muller <smuller@tequilarapido.com>
2014-06-03 10:54:32 +0200

    Add getMovies, addMovie, rateMovie actions

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var express   = require('express');
-var Datastore = require('nedb');
+var datastore = require('nedb');
 
 // ------------------------------------------------------------------------------------------------
 
 var app = express();
-
 app.use(express.bodyParser());
 
 //app.use(express.json());
@@ -12,23 +11,42 @@ app.use(express.bodyParser());
 
 // ------------------------------------------------------------------------------------------------
 
-var db    = {};
-db.movies = new Datastore({ filename: 'db/movies', autoload: true });
+var db = {};
+db.movies = new datastore({ filename: 'db/movies', autoload: true });
 
 // ------------------------------------------------------------------------------------------------
 // ------------------------------------------------------------------------------------------------
 // ------------------------------------------------------------------------------------------------
 
-app
-.get('/', function(req, res) {
-	res.send("The API is working.");
+app.get('/', function (req, res) {
+    res.send("The API is working.");
 })
-.post('/rpc', function(req, res) {
-	var body = req.body;
-	switch (body.action) {
-		case 'getMovies':
-			res.send("Requested API getMovie");
-			break;
-	}
+.post('/rpc', function (req, res) {
+    var body = req.body;
+
+    res.set('Content-type', 'application/json');
+    var respond = function(error, results) {
+        if (error) {
+            res.send(JSON.stringify(error));
+        } else {
+            res.send(JSON.stringify(results));
+        }
+    };
+
+    switch (body.action) {
+        case 'getMovies':
+            db.movies.find({}, respond);
+            break;
+        case 'addMovie':
+            db.movies.insert({ title: body.title }, respond);
+            break;
+        case 'rateMovie':
+            db.movies.update({ title: body.title }, { $set: { rating: body.rating } }, function(error, num) {
+                respond(error, { success: num + " record" + ((num > 1) ? "s" : "") + " updated" });
+            });
+            break;
+        default:
+            respond("RPC No action given");
+    }
 })
 .listen(3000);
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Hypermedia & REST w/ Node Express
+
+## Installation
+
+ * npm install
+ * node index.js
+
+## Nodemon
+
+It will be better to use `nodemon`
+
+ * npm install -g nodemon
+ * nodemon index.js
